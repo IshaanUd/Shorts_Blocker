b18ad076fb8c024ccfa33aa6bc160fe1e36db35d
Ishaan2 <udia@wisc.edu>
2024-07-28 16:25:48 -0500

    added changes to the content.js file, made two new files

--- content.js
+++ content.js
@@ -1,15 +1,26 @@
-function isYouTubeShort() {
-    //detects whether or not
-}
-function checkCurrentPage() {
-    // checks current page, if it is a youtube short, then it 
-}
-function displayOutputMessage() {
-    // function triggered only when a user clicks on youtube shorts
-    //intended to displaymessage and time spent on watching youtueb shorts 
-}
-function displayOutputMessage() {
-    // function triggered only when a user clicks on youtube shorts
-    //intended to displaymessage and time spent on watching youtueb shorts 
-}
-checkCurrentPage();
+(function () {
+    // Function to block YouTube Shorts
+    function blockYouTubeShorts() {
+        // Select elements that represent YouTube Shorts, retrieved from internet
+        // ytd-reel-shelf-renderer  -  for reels on home page
+        // ytd-rich-sheld-rendere - for reels that come up on search page
+        var shortsElements = document.querySelectorAll('ytd-reel-shelf-renderer, ytd-rich-shelf-renderer');
+        // implement for every element in the  
+        for (var i = 0; i < shortsElements.length; i++) {
+            var el = shortsElements[i];
+            el.style.display = 'none';
+        }
+    }
+    var mutationCallback = function (mutations, observer) {
+        mutations.forEach(function (mutation) {
+            if (mutation.type === "childList") {
+                blockYouTubeShorts();
+            }
+        });
+    };
+    var observer = new MutationObserver(mutationCallback);
+    // Start observing changes to the document body and its descendants
+    observer.observe(document.body, { childList: true, subtree: true });
+    // Initial block of YouTube Shorts
+    blockYouTubeShorts();
+})();
